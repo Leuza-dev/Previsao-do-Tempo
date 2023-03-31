e1a3c26a447abd9216b54be0399ccbef80e0af03
Leuza Lima <leuzadev@gmail.com>
2023-03-31 14:10:54 -0300

    First commit

---
+++ scripts.js
@@ -0,0 +1,39 @@
+
+
+
+const key = "ec80621f1673f9b0ee29ab9d5000d90d"
+
+
+function colocarDadosNaTela(dados) {
+
+    console.log(dados)
+
+    document.querySelector(".cidade").innerHTML = "Tempo em " + dados.name
+
+    document.querySelector(".temp").innerHTML = Math.floor(dados.main.temp) + "°C"
+    document.querySelector(".texto-previsao").innerHTML = "Tempo: " + dados.weather[0].description 
+    document.querySelector(".umidade").innerHTML = "Umidade Relativa do Ar: " + dados.main.humidity +  "%"
+    document.querySelector(".img-previsao").src = `https://openweathermap.org/img/wn/${dados.weather[0].icon}.png`
+ 
+
+
+}
+
+
+async function buscarCidade(cidade) {
+
+    const dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}&lang=pt_br&units=metric`).then(resposta => resposta.json())
+    colocarDadosNaTela(dados)
+
+
+
+}
+
+function cliqueiNoBotao() {
+
+    const cidade = document.querySelector(".input-cidade").value
+
+    buscarCidade(cidade)
+
+
+}
